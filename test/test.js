ba9dcef2f9fc310257b7017f793632de39fbd2cf
Gianluca Pengo <gianluca.pengo@gmail.com>
2014-12-23 13:47:10 +0100

    add connection, errors, collection

---
+++ test/test.js
@@ -0,0 +1,19 @@
+var docooment = require('../index'),
+	Schema = docooment.Schema;
+
+
+docooment.connect('https://test-kademy.documents.azure.com', 443, 'test-kademy' , {masterKey: 'hSui6X08mjlXQnTzsvUfLHIV56+OdNemziePwc/iqljXvRRGdLUOz2DZIye1ZB1/fv1PAargsR7UElTg7sJD8A=='});//callback
+/*
+var UserSchema = new Schema({   
+	email: { type: String, lowercase: true },
+	firstName: String,
+	lastName: String,
+});
+
+var User = docooment.model('User', UserSchema);
+
+var user = new User({firstName: 'Gianluca'});
+user.save(function (err) {
+  if (err) console.error(err)
+  console.log('user',user);
+});*/
