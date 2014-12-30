441421baa56c8f187d1846806f2c5214c12b0b8d
Gianluca Pengo <gianluca.pengo@gmail.com>
2014-12-30 16:42:38 +0100

    create collection on insert first element

--- test/test.js
+++ test/test.js
@@ -2,10 +2,10 @@ var docooment = require('../index'),
 	Schema = docooment.Schema;
 
 
-docooment.connect('https://test-kademy.documents.azure.com', 443, 'test-kademy' , {masterKey: 'hSui6X08mjlXQnTzsvUfLHIV56+OdNemziePwc/iqljXvRRGdLUOz2DZIye1ZB1/fv1PAargsR7UElTg7sJD8A=='}, function(err){
+docooment.connect('https://test-kademy.documents.azure.com', 443, 'kademy' , {masterKey: 'hSui6X08mjlXQnTzsvUfLHIV56+OdNemziePwc/iqljXvRRGdLUOz2DZIye1ZB1/fv1PAargsR7UElTg7sJD8A=='}, function(err){
 	if(err) return console.error(err);
 	docooment.set('debug', true)
-	
+
 	console.log('CONNECTED')
 
 	var UserSchema = new Schema({   
@@ -14,33 +14,44 @@ docooment.connect('https://test-kademy.documents.azure.com', 443, 'test-kademy' 
 		lastName: String,
 	});
 
-
 	var User = docooment.model('User', UserSchema);
+
+	var CertificateSchema = new Schema({   
+		title: { type: String},
+	});
+
+	var Certificate = docooment.model('Certificate', CertificateSchema);
 
 	var user = new User({firstName: 'Gianluca'});
 	//console.log(user);
 	
 	user.save(function (err) {
 	  if (err) console.error(err)
-	  // 	console.log('user', user);
+	   	console.log('user', user);
 
 		User.find({firstName: 'Gianluca'}).limit(5).sort({id:1}).exec(function(err, users){
 			if(err) return console.error(err);
 			//console.log(users)
 			console.log(users[0])
-			User.remove({_id: users[0]}, function(err, results){
-				if(err) console.error(err);
-				console.log('deleted', results);
+			
+			User.update({_id: users[0]._id}, {lastName: 'Pengo'}, function(err, results){
+				if(err) return console.error(err);
+				console.log('updated', results);
 
-				User.update({_id: users[1]._id}, {lastName: 'Pengo'}, function(err, results){
-					if(err) console.error(err);
-					console.log('updated', results);
+				users[0].lastName = 'Rossi';
+				users[0].save(function(err, results){
+					if(err) return console.error(err);
 
-					users[2].lastName = 'Rossi';
-					users[2].save(function(err, results){
+					console.log('saved', results)
+
+					User.remove({_id: users[0]}, function(err){
 						if(err) return console.error(err);
+						console.log('deleted');
 
-						console.log(results)
+
+						Certificate.create({title: 'prova'}, function(err, certificate){
+							console.log(err, certificate);
+						});
 					});
 				});
 			});
