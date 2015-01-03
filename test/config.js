a90c24d673c817ba64cb9418d243cc1a0168f698
Gianluca Pengo <gianluca.pengo@gmail.com>
2015-01-03 15:46:16 +0100

    travis cir

--- test/config.js
+++ test/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	uri: 'https://your-document-account.documents.azure.com',
+	uri: 'https://your-db-account.documents.azure.com',
 	port: 443,
 	database: 'yourDatabase',
 	options: {masterKey: 'yourMasterKey'}
